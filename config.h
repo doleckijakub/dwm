f5c9018637554ed624b5b29a90de91b7c01d988e
Jakub Dolecki <doleckijakub@users.noreply.github.com>
2024-01-12 14:03:24 +0100

    Applied patch dwm-cfacts-20200913-61bb8b2

--- config.h
+++ config.h
@@ -71,6 +71,8 @@ static const Key keys[] = {
 	// { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = -0.05} },
+	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_d,      spawn,          SHCMD("rofi -show run") },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
