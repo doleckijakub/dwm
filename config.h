23a64c05c14e9f0bcecba296bf31cbbfd07c8840
Jakub Dolecki <doleckijakub@users.noreply.github.com>
2024-01-31 14:36:46 +0100

    Make PacketTracer windows floating

--- config.h
+++ config.h
@@ -7,11 +7,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#222222"; // status bar unfocused
+static const char col_gray2[]       = "#444444"; // unfocused window border
+static const char col_gray3[]       = "#bbbbbb"; // bar text unfocused
+static const char col_gray4[]       = "#eeeeee"; // bar text focused
+static const char col_cyan[]        = "#1E1E2E"; // status bar focused
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
@@ -38,6 +38,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "PacketTracer", NULL,   NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
